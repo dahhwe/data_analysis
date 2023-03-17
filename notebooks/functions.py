b37f9d80eb3e2ec0e71317e5cda43b6e74f61722
Dani <dahhwe@gmail.com>
2023-03-17 22:56:18 +0700

    Complete
    Final.

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,9 +1,10 @@
 """Основные функции для построения графиков"""
-from typing import List, Any
 
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from scipy.stats import sigmaclip
+import seaborn as sns
 
 
 def draw_charts(dataframe: pd.DataFrame, column: str, var: str, fig_size=(15, 5)) -> None:
@@ -28,6 +29,41 @@ def draw_charts(dataframe: pd.DataFrame, column: str, var: str, fig_size=(15, 5)
     axs[1].set(title=f"Круговая диаграмма распределения {var} в выборке")
 
     plt.show()
+
+
+def quartile_method(quartile_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Метод квартилей
+    :param quartile_df: датафрейм для применения метода квартилей
+    :return: измененный датафрейм, с отсеченными значениями
+    """
+    num_cols = quartile_df.select_dtypes(include=['float']).columns
+    q1 = quartile_df[num_cols].quantile(0.25)
+    q3 = quartile_df[num_cols].quantile(0.75)
+    iqr = q3 - q1
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
+    filtered_df = quartile_df[
+        ~((quartile_df[num_cols] < lower_bound) | (quartile_df[num_cols] > upper_bound)).any(axis=1)]
+    filtered_df = filtered_df.reset_index(drop=True)
+    return filtered_df
+
+
+def sigma_method(sigma_df: pd.DataFrame):
+    """
+    Метод сигм
+    :param sigma_df: датафрейм для применения метода сигм
+    :return: измененный датафрейм, с отсеченными значениями
+    """
+    numerical_column = sigma_df.select_dtypes(include=['float']).columns
+    for column in numerical_column:
+        data = sigma_df[column].dropna()
+        clean_data, low, high = sigmaclip(data, low=3, high=3)
+        sigma_df = sigma_df.loc[(sigma_df[column].isin(clean_data)) | (sigma_df[column].isna())]
+
+    sigma_df = sigma_df.reset_index()
+    sigma_df.pop('index')
+    return sigma_df
 
 
 def draw_histogram(dataframe: pd.DataFrame, column: str, number: int) -> None:
@@ -64,9 +100,67 @@ def remove_outliers(dataframe: pd.DataFrame, column: str) -> pd.DataFrame:
     return dataframe
 
 
-def quartile_method(dataframe: pd.DataFrame, column: str) -> list[float | Any]:
-    q25, q75 = dataframe[column].quantile([0.25, 0.75])
-    delta = q75 - q25
-    mild_border = [q25 - 1.5 * delta, q75 + 1.5 * delta]  # Незначительный разброс
-    extreme_border = [q25 - 3 * delta, q75 + 3 * delta]  # Значительный разброс
-    return mild_border
+def visualisation(df, data_type, data_cols, data_titles=None):
+    def visual_func(data, title):
+        fig, axes = plt.subplots(1, 2, constrained_layout=True, figsize=(12, 6))
+        fig.suptitle(title, fontsize=20)
+
+        if data_type == 'qualitative':
+            counts = data.value_counts()
+            bar_axe, pie_axe = axes
+
+            bar_axe.bar(counts.index, counts.values)
+            bar_axe.set_xticks(range(len(counts.index)))
+            bar_axe.set_xticklabels(counts.index)
+            bar_axe.set_xlabel('Варианты')
+            bar_axe.set_ylabel('Количество')
+            bar_axe.set_title('Столбчатая диаграмма')
+
+            pie_axe.pie(counts.values, labels=counts.index, autopct='%1.2f%%')
+            pie_axe.set_title('Круговая диаграмма')
+        else:
+            sns.histplot(data, kde=True, ax=axes[0])
+            axes[0].set_xlabel('Значения')
+            axes[0].set_ylabel('Относительная плотность')
+            axes[0].set_title('Гистограмма с графиком функции распределения')
+
+            sns.boxplot(data, ax=axes[1], orient='h')
+            axes[1].set_xlabel('Значения')
+            axes[1].set_ylabel('')
+            axes[1].set_title('Ящик с усами')
+
+        plt.show()
+
+    if data_titles is None:
+        data_titles = data_cols
+
+    for col, title in zip(data_cols, data_titles):
+        visual_func(df[col], title)
+
+
+def draw_quantitative(
+    series: pd.Series,
+    title: str,
+    x_label: str,
+    kde_bandwidth: float = 0.5,
+    boxplot_orientation: str = 'h'
+) -> None:
+    """
+    Function to draw quantitative plots
+    :param series: Series to draw
+    :param title: Plot title
+    :param x_label: Label for the x-axis
+    :param kde_bandwidth: Bandwidth for the kernel density estimation
+    :param boxplot_orientation: Orientation of the boxplot
+    """
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, constrained_layout=True, figsize=(12, 5), dpi=100)
+    fig.suptitle(title, fontsize=20)
+    ax1.set_ylabel('Relative frequency density')
+    ax1.set_xlabel(x_label)
+    ax2.set_ylabel('Relative frequencies')
+    ax2.set_xlabel(x_label)
+    ax3.set_xlabel('Values')
+    ax1.hist(series)
+    sns.kdeplot(data=series, bw_method=kde_bandwidth, ax=ax2)
+    sns.boxplot(data=series, ax=ax3, orient=boxplot_orientation)
+    plt.show()
